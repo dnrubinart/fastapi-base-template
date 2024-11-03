087ade252095fef79650fe43e77c9d663e690ffc
Rubinart <k.ivanov19981@gmail.com>
2024-11-03 13:55:45 +0200

    Config set up

---
+++ src/core/config.py
@@ -0,0 +1,18 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = ""
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    DATABASE_URI: str = os.getenv("DATABASE_URI")
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
