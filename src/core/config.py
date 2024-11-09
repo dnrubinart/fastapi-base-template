2c79f896a057216d71de9071f1866809acabbbb4
Rubinart <k.ivanov19981@gmail.com>
2024-11-09 23:57:41 +0200

    Config configured

--- src/core/config.py
+++ src/core/config.py
@@ -1,12 +1,28 @@
 import os
+from functools import lru_cache
+from typing import List, Union
 
-from pydantic import BaseSettings
+from pydantic import field_validator
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str = ""
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    PROJECT_NAME: str = "App"
+
+    CORS_ALLOWED_HOSTS: list[str] = ["*"]
+
+    @field_validator("CORS_ALLOWED_HOSTS", check_fields=False)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        else:
+            return v
+
+    JWT_SECRET_KEY: str = os.getenv("SECRET_KEY")
+    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")
+    JWT_EXPIRATION: int = os.getenv("JWT_EXPIRATION")
+
     DATABASE_URL: str = os.getenv("DATABASE_URL")
 
     class Config:
@@ -15,4 +31,9 @@ class Settings(BaseSettings):
         env_file_encoding = "utf-8"
 
 
-settings = Settings()
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
