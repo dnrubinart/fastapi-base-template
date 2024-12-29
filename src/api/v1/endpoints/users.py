8bf8b0f3bc8f4b587315af10f42af96d383f6c41
Rubinart <k.ivanov19981@gmail.com>
2024-12-30 00:36:59 +0200

    example: async crud/route

--- src/api/v1/endpoints/users.py
+++ src/api/v1/endpoints/users.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.crud.user import read_users
+from ...deps import get_db
 
 router = APIRouter()
 
 
 @router.get("/")
-def read_users():
-    pass
+async def get_users(db: AsyncSession = Depends(get_db)):
+    """
+    Fetch all users.
+    """
+    return await read_users(db)
